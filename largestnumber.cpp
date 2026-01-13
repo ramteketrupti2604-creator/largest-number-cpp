2ad0b09de5e9c1e314e66a10d94b55e943d29ac1
ramteketrupti2604-creator <ramteketrupti2604@gmail.com>
2026-01-13 14:08:04 +0530

    Add files via upload

---
+++ largestnumber.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a =22;
+    int b= 33;
+    if(a<b){
+        cout<<" b is large";
+    }
+    else{
+        cout<<" a is large";
+    }
+    return 0;
+}
